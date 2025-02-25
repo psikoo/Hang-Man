1e4bc24e05f469ce1623a4c3d89d9261afbeeb9c
IUTIUB <jonzalomateossanchez@gmail.com>
2025-02-25 11:52:08 +0100

    Update Man.java

--- src/main/java/com/hangman/Man.java
+++ src/main/java/com/hangman/Man.java
@@ -1,5 +1,25 @@
 package com.hangman;
 
 public class Man {
+    public static void hombrecillo() {
+        String[] estados = {
+            "   +---+\n   |   |\n       |\n       |\n       |\n       |\n =========\n",
+            "   +---+\n   |   |\n   O   |\n       |\n       |\n       |\n =========\n",
+            "   +---+\n   |   |\n   O   |\n   |   |\n       |\n       |\n =========\n",
+            "   +---+\n   |   |\n   O   |\n  /|   |\n       |\n       |\n =========\n",
+            "   +---+\n   |   |\n   O   |\n  /|\\  |\n       |\n       |\n =========\n",
+            "   +---+\n   |   |\n   O   |\n  /|\\  |\n  /    |\n       |\n =========\n",
+            "   +---+\n   |   |\n   O   |\n  /|\\  |\n  / \\  |\n       |\n =========\n",
+            "   +---+\n   |   |\n   O  |\n  /|\\  |\n  / \\  |\n       |\n =========\n"
+        };
+        
+        for (String estado : estados) {
+            System.out.println(estado);
+            System.out.println();
+        }
+    }
     
+    public static void main(String[] args) {
+        hombrecillo();
+    }
 }
