ffd1e04f85d5c36b4ff61e412d9252b8bdf2d972
psikoo <hugohernandezmuelas03@gmail.com>
2025-02-25 11:09:46 +0100

    stage1

--- src/main/java/com/hangman/Main.java
+++ src/main/java/com/hangman/Main.java
@@ -2,7 +2,7 @@ package com.hangman;
 
 public class Main {
     public static void main( String[] args ) { // mvn clean validate compile site assembly:assembly -DdescriptorId=jar-with-dependencies exec:exec
-        System.out.println("\n > Your build process is working, YabaaDabaaDoo!");
-        System.out.println(" > Now go create something great :3");
+        new Menu();
+        new Game();
     }
 }
