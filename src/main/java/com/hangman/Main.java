b45ed0c0de8ba13cabc2f764722511ac1f3885bc
psikoo <hugohernandezmuelas03@gmail.com>
2025-04-07 09:54:42 +0200

    added  500 pass

--- src/main/java/com/hangman/Main.java
+++ src/main/java/com/hangman/Main.java
@@ -1,7 +1,9 @@
 package com.hangman;
 
 public class Main {
-    public static void main( String[] args ) { // mvn clean validate compile site assembly:assembly -DdescriptorId=jar-with-dependencies exec:exec
+    public static void main( String[] args ) { 
+        // mvn clean validate compile site assembly:assembly -DdescriptorId=jar-with-dependencies exec:exec
+        // sudo mvn clean validate compile site assembly:assembly -DdescriptorId=jar-with-dependencies
         // Ansi check
         System.out.println("\nIf you are seeing this run the following command and rerun the program (⚠Windows)");
         System.out.println("reg add HKEY_CURRENT_USER\\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000001 /f");
